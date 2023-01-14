845a674e18ae6620263bf3f2b0cac8fe1f7f10f3
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-14 10:22:00 +0530

    first commit

---
+++ src/test/java/com/example/springbootcrud/SpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
