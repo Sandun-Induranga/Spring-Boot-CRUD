81bce440988f4eda3650a0d1b4002678c278417f
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-14 11:41:53 +0530

    Post Mapping For Save Customer

--- src/main/java/com/example/springbootcrud/controller/CustomerController.java
+++ src/main/java/com/example/springbootcrud/controller/CustomerController.java
@@ -1,9 +1,26 @@
 package com.example.springbootcrud.controller;
 
+import com.example.springbootcrud.entity.Customer;
+import com.example.springbootcrud.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
 
+@RestController
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping("/saveCustomer")
+    public boolean saveCustomer(@RequestBody Customer customer){
+        return customerService.saveCustomer(customer);
+    }
+
 }
