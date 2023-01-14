d0ab5d26d8ed587a337cae365626df66e1727469
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-14 10:31:10 +0530

    Student Class Completed

--- src/main/java/com/example/springbootcrud/entity/Student.java
+++ src/main/java/com/example/springbootcrud/entity/Student.java
@@ -1,9 +1,26 @@
 package com.example.springbootcrud.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue
+    private int studentId;
+    private String name;
+    private String address;
+    private int age;
 }
