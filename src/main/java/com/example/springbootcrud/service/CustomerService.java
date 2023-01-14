aab5efd6b5feb2c38037fc5177a6ef79f5a4e12c
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-14 11:20:05 +0530

    Save Customer

--- src/main/java/com/example/springbootcrud/service/CustomerService.java
+++ src/main/java/com/example/springbootcrud/service/CustomerService.java
@@ -1,9 +1,24 @@
 package com.example.springbootcrud.service;
 
+import com.example.springbootcrud.entity.Customer;
+import com.example.springbootcrud.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
 
+@Service
 public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+    public Customer saveCustomer(Customer customer){
+        return customerRepository.save(customer);
+    }
+
 }
