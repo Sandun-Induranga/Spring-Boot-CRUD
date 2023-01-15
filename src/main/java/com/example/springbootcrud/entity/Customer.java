366bad09acff59fcb8474e0a842a4e3b818ebf90
Sandun Induranga <88975401+Sandun-Induranga@users.noreply.github.com>
2023-01-15 23:34:20 +0530

    Application Completed

--- src/main/java/com/example/springbootcrud/entity/Customer.java
+++ src/main/java/com/example/springbootcrud/entity/Customer.java
@@ -1,8 +1,6 @@
 package com.example.springbootcrud.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
